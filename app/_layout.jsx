0ea22c894bee0ac27a9bfc21fafacaaca18438c6
Volodymyr Basok <volodymyrquo@gmail.com>
2024-04-23 17:48:12 +0300

    add authorization

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
+import GlobalProvider from '../context/GlobalProvider';
+
 SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
@@ -20,9 +22,17 @@ const RootLayout = () => {
   },[fontsLoaded, error])
 
   if(!fontsLoaded && !error) return null;
-   return <Stack>
+   return ( 
+   <GlobalProvider>
+       <Stack>
     <Stack.Screen name='index' options={{headerShown:false}} />
+    <Stack.Screen name='(auth)' options={{headerShown:false}} />
+    <Stack.Screen name='(tabs)' options={{headerShown:false}} />
+    {/* <Stack.Screen name='/search/[query]' options={{headerShown:false}} /> */}
   </Stack>
+
+   </GlobalProvider>
+   )
 }
 
 export default RootLayout
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
-
+import { Image, ScrollView, Text, View } from 'react-native';
+import { router, Redirect } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
+import {useGlobalContext} from '../context/GlobalProvider'
 export default function App() {
+  const {isLoading,isLoggedIn}=useGlobalContext();
+  console.log(isLoading,isLoggedIn)
+  if(!isLoading && isLoggedIn) return <Redirect href='/home' />
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href='/profile' style={{color:'blue'}}>Go to Profile</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+<ScrollView contentContainerStyle={{height:'100%'}} >
+<View className='w-full justify-center items-center min-h-[85vh] px-4' >
+  <Image  source={images.logo} 
+  className="w-[130px] h-[84px]"
+  resizeMode='contain'
+  />
+  <Image  source={images.cards} 
+  className="max-w-[380px] w-full h-[300px]"
+  resizeMode='contain'
+  />
+  <View className="relative mt-5">
+    <Text className="text-2xl text-white font-bold text-center">
+Discover Endless Possibilities with{' '}<Text className="text-secondary-200">Aora</Text>
+    </Text>
+    <Image source={images.path} className="w-[136px h-[15px] absolute -bottom-2 -right-16" resizeMode='contain' />
+  </View>
+<Text
+className='text-sm font-pregular text-gray-100 mt-10 text-center'
+>Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+<CustomButton title="Continue with Email" handlePress={()=>router.push('/sign-in')} containerStyles="w-full mt-7" />
+</View>
+
+</ScrollView>
+<StatusBar backgroundColor='#161622' style='light'/>
+    </SafeAreaView>
   );
 }
 
