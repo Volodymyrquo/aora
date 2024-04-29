cf64f84d73d157d48e7793b34e2cba236d1432c6
Volodymyr Basok <volodymyrquo@gmail.com>
2024-04-29 14:10:53 +0300

    add video

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -13,11 +13,12 @@ import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
 import EmptyState from '../../components/EmptyState';
-import { getAllPosts } from '../../lib/appwrite';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard';
 const Home = () => {
     const { data: posts, refetch } = useAppwrite(getAllPosts);
+    const { data: latestPosts } = useAppwrite(getLatestPosts);
 
     const [refreshing, setRefreshing] = useState(false);
     const onRefresh = async () => {
@@ -33,9 +34,7 @@ const Home = () => {
             <FlatList
                 data={posts}
                 keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <VideoCard video={item} />
-                )}
+                renderItem={({ item }) => <VideoCard video={item} />}
                 ListHeaderComponent={() => (
                     <View className="my-6 px-4 space-y-6">
                         <View className="justify-between items-start flex-row mb-6">
@@ -60,9 +59,7 @@ const Home = () => {
                             <Text className="text-gray-100 text-lg font-pregular mb-3">
                                 Latest Videos
                             </Text>
-                            <Trending
-                                posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []}
-                            />
+                            <Trending posts={latestPosts ?? []} />
                         </View>
                     </View>
                 )}
